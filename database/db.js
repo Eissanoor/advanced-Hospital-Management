60b0de1415e322de11d011c2b562aa90fa890a22
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2022-11-21 00:01:45 -0800

    asdasd

--- database/db.js
+++ database/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 dotenv.config({ path: "./config.env" });
 
-const MONGODB_URL = "mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net/humstaffing?retryWrites=true&w=majority";
+const MONGODB_URL =
+  "mongodb+srv://eissanoor:Eisa.123@cluster0.bpuor.mongodb.net/humstaffing?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGODB_URL, {
